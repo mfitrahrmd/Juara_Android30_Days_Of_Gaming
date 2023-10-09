1399554aa3d1a45c4ea3df38d82696ceb48f7ab2
mfitrahrmd <tgcfitrah26@gmail.com>
2023-10-09 17:29:35 +0700

    define shape

---
+++ app/src/main/java/com/mfitrahrmd/juara_android_30_days_of_gaming/ui/theme/Shape.kt
@@ -0,0 +1,10 @@
+package com.mfitrahrmd.juara_android_30_days_of_gaming.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val shape = Shapes(
+    small = RoundedCornerShape(8.dp),
+    medium = RoundedCornerShape(16.dp)
+)
