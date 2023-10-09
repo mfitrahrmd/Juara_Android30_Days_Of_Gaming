d9bd6e03566cb5b7565a1666df0874431a0e60ec
mfitrahrmd <tgcfitrah26@gmail.com>
2023-10-09 12:17:31 +0700

    add font

--- app/src/main/java/com/mfitrahrmd/juara_android_30_days_of_gaming/ui/theme/Type.kt
+++ app/src/main/java/com/mfitrahrmd/juara_android_30_days_of_gaming/ui/theme/Type.kt
@@ -2,33 +2,38 @@ package com.mfitrahrmd.juara_android_30_days_of_gaming.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.mfitrahrmd.juara_android_30_days_of_gaming.R
+
+val Rubik = FontFamily(
+    Font(R.font.rubik_regular),
+    Font(R.font.rubik_bold, FontWeight.Bold),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Rubik,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Rubik,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
+        fontFamily = Rubik,
+        fontWeight = FontWeight.Bold,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
